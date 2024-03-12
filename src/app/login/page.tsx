6a2e55169fdd30882b2c6259d4bb8f1a8168fb9c
Rael2003 <rsouza2123@gmail.com>
2024-03-12 14:14:34 -0300

    login-screen

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "../styles.module.css"
+import Login from "./login.module.css"
+
+export default function login(){
+    return (
+        <div className={styles.tela}>
+            <div className={Login.imgs}></div>
+            <div className={Login.allright}>
+                <h1 className={Login.title}> Login</h1>
+                <input className={styles.text} type="text" id="email" placeholder="E-mail"></input>
+                <input className={styles.text} type="password" id="Senha" placeholder="Senha"></input>
+                <button className={styles.button}> Entrar</button>
+            </div>
+        </div>
+        
+    );
+}
